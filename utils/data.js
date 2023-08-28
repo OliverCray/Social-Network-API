f2df4dc825f50ceb70c82a4acdb44f15677a87b9
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-08-28 17:18:35 +0100

    Added seed data and ability to seed

---
+++ utils/data.js
@@ -0,0 +1,128 @@
+const usernames = [
+  'CoolCat',
+  'GamerGirl123',
+  'FoodieAdventures',
+  'TravelBuddy',
+  'FitnessJunkie',
+  'TechWizard',
+  'BookwormLife',
+  'Fashionista',
+  'MusicLover22',
+  'ArtisticSoul',
+  'WanderlustSpirit',
+  'CoffeeAddict',
+  'MovieBuff',
+  'PetLover365',
+  'NatureExplorer',
+  'DIYEnthusiast',
+  'FoodieEats',
+  'PositiveVibesOnly',
+  'FitnessGuru',
+  'StarGazer',
+  'AdventureSeeker',
+  'FashionFrenzy',
+  'TechGeek',
+  'BookNerd',
+  'MusicMania',
+  'CreativeDreamer',
+  'SunnyDaysAhead',
+  'MindfulTraveler',
+  'GamingLegend',
+  'CookingMaestro',
+  'AnimalAdventures',
+  'OutdoorExplorer',
+  'CraftyCreator',
+  'MovieMaven',
+  'PetPal',
+  'ArtisticSoul',
+  'WanderlustJourney',
+  'CaffeineQueen',
+  'Cinephile',
+  'YogaEnthusiast',
+  'LifeHacks101',
+  'GreenThumb',
+  'WittyWordsmith',
+  'FitnessFreak',
+  'AstroExplorer',
+  'UrbanAdventures',
+  'FashionForward',
+  'TechInnovator',
+  'LiteraryLover',
+  'MelodyMaker',
+  'ImaginationUnleashed',
+]
+
+const thoughts = [
+  "I'm feeling great today!",
+  'What a wonderful day!',
+  'Feeling a bit tired...',
+  "Can't wait for the weekend!",
+  'Feeling inspired!',
+  'Today is a productive day.',
+  'Feeling a little under the weather.',
+  'Excited for the upcoming event!',
+  'Feeling motivated!',
+  'Just taking things one step at a time.',
+  'Feeling grateful for everything.',
+  'Feeling a bit overwhelmed.',
+  'Enjoying some quiet time.',
+  'Feeling on top of the world!',
+  'Hoping for the best.',
+  'Feeling adventurous!',
+  "Today's a day for relaxation.",
+  'Feeling creative and artistic.',
+  'Missing old friends today.',
+  'Feeling curious about the unknown.',
+]
+
+const reactions = [
+  '👍',
+  '❤️',
+  '😄',
+  '😊',
+  '😎',
+  '🤔',
+  '😅',
+  '😢',
+  '😂',
+  '😇',
+  '😌',
+  '🙌',
+  '😋',
+  '🥳',
+  '🤩',
+  '😓',
+  '🥺',
+  '🤗',
+  '🙏',
+  '😬',
+]
+
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)]
+
+const getRandomUsername = () => getRandomArrItem(usernames)
+
+const getRandomReactions = (int) => {
+  const results = []
+  for (let i = 0; i < int; i++) {
+    results.push({
+      reactionBody: getRandomArrItem(reactions),
+      username: getRandomUsername(),
+    })
+  }
+  return results
+}
+
+const getRandomThoughts = (int) => {
+  const results = []
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtText: getRandomArrItem(thoughts),
+      username: getRandomUsername(),
+      reactions: getRandomReactions(Math.floor(Math.random() * 6)),
+    })
+  }
+  return results
+}
+
+module.exports = { getRandomUsername, getRandomThoughts }
---
+++ utils/seed.js
@@ -0,0 +1,47 @@
+const connection = require('../config/connection')
+const { User, Thought } = require('../models')
+const { getRandomUsername, getRandomThoughts } = require('./data')
+
+connection.on('error', (err) => err)
+
+connection.once('open', async () => {
+  console.log('connected')
+
+  let thoughtCheck = await connection.db
+    .listCollections({ name: 'thoughts' })
+    .toArray()
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts')
+  }
+
+  let userCheck = await connection.db
+    .listCollections({ name: 'users' })
+    .toArray()
+  if (userCheck.length) {
+    await connection.dropCollection('users')
+  }
+
+  const users = []
+
+  for (let i = 0; i < 20; i++) {
+    const username = getRandomUsername()
+
+    users.push({
+      username,
+      email: `${username.toLowerCase().split(' ').join('')}@gmail.com`,
+    })
+  }
+
+  const thoughts = getRandomThoughts(20)
+
+  await User.collection.insertMany(users)
+
+  await Thought.collection.insertMany(thoughts)
+
+  console.table(users)
+  console.table(thoughts)
+
+  console.info('Seeding complete! 🌱')
+
+  process.exit(0)
+})
